31edd5d5d82d288c23fbd545e3a82dd10d6dae48
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-06-13 12:01:38 +0200

    Run "done" method when parser is done

--- source/php/Parser.php
+++ source/php/Parser.php
@@ -10,10 +10,16 @@ abstract class Parser
     {
         $this->url = $url;
         $this->start();
+        $this->done();
     }
 
     /**
      * Used to start the parsing
      */
     abstract public function start();
+
+    public function done()
+    {
+        echo 'Parser done.';
+    }
 }
