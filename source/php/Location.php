3f0364d3feda39eefabc2f0f37b933f11f55b1d9
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-06-13 15:06:28 +0200

    Try to find coordinates from address via google

--- source/php/Location.php
+++ source/php/Location.php
@@ -5,4 +5,15 @@ namespace HbgEventImporter;
 class Location extends \HbgEventImporter\Entity\PostManager
 {
     public $postType = 'location';
+
+    public function afterSave()
+    {
+        $coordinates = Helper\Address::getCoordinatesByAddress($this->postalAddress . ' ' . $this->postcode . ' ' . $this->city);
+
+        update_post_meta($this->ID, 'map', array(
+            'address' => $this->postalAddress . ', ' . $this->postcode . ' ' . $this->city . ', ' . $this->country,
+            'lat' => $coordinates->lat,
+            'lng' => $corrdinates->lng
+        ));
+    }
 }
