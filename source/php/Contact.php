170364b76ecd985ebe5865e2ea5b8cb6b8c11cd8
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-06-14 15:26:08 +0200

    Phone number formatting

--- source/php/Contact.php
+++ source/php/Contact.php
@@ -2,7 +2,19 @@
 
 namespace HbgEventImporter;
 
+use \HbgEventImporter\Helper\DataCleaner as DataCleaner;
+
 class Contact extends \HbgEventImporter\Entity\PostManager
 {
     public $post_type = 'contact';
+
+    /**
+     * Stuff to do before save
+     * @return void
+     */
+    public function beforeSave()
+    {
+        // Format phone number
+        $this->phoneNumber = DataCleaner::phoneNumber($this->phoneNumber);
+    }
 }
--- source/php/Event.php
+++ source/php/Event.php
@@ -2,9 +2,21 @@
 
 namespace HbgEventImporter;
 
+use \HbgEventImporter\Helper\DataCleaner as DataCleaner;
+
 class Event extends \HbgEventImporter\Entity\PostManager
 {
     public $post_type = 'event';
+
+    /**
+     * Stuff to do before save
+     * @return void
+     */
+    public function beforeSave()
+    {
+        // Format phone number
+        $this->phoneNumber = DataCleaner::phoneNumber($this->phoneNumber);
+    }
 
     /**
      * Stuff to do after save
