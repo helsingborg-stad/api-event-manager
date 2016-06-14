b60940b4773b39d97315c62fa872ade1ceebb027
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-06-14 14:23:28 +0200

    Save categories as event-categories terms

--- source/php/Event.php
+++ source/php/Event.php
@@ -5,4 +5,22 @@ namespace HbgEventImporter;
 class Event extends \HbgEventImporter\Entity\PostManager
 {
     public $post_type = 'event';
+
+    /**
+     * Stuff to do after save
+     * @return void
+     */
+    public function afterSave()
+    {
+        $this->saveCategories();
+    }
+
+    /**
+     * Saves categories as event-categories taxonomy terms
+     * @return void
+     */
+    public function saveCategories()
+    {
+        wp_set_object_terms($this->ID, $this->categories, 'event-categories', true);
+    }
 }
