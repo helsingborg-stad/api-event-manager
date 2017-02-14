384d051f376e0ef3f895cdf22e3f63fb65f65ba8
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-02-14 14:25:19 +0100

    merge

--- source/php/Acf/AcfGuideSettings.php
+++ source/php/Acf/AcfGuideSettings.php
@@ -18,52 +18,6 @@ class AcfGuideSettings
         if (function_exists('acf_add_local_field_group')):
 
         acf_add_local_field_group(array(
-            'key' => 'group_589dd0fbd412e',
-            'title' => __('Guide apperance', 'event-manager'),
-            'fields' => array(
-                array(
-                    'taxonomy' => 'guide_sender',
-                    'field_type' => 'select',
-                    'multiple' => 0,
-                    'allow_null' => 0,
-                    'return_format' => 'object',
-                    'add_term' => 1,
-                    'load_terms' => 0,
-                    'save_terms' => 0,
-                    'key' => 'field_589dd138aca7e',
-                    'label' => __('Select apperance', 'event-manager'),
-                    'name' => 'guide_apperance_data',
-                    'type' => 'taxonomy',
-                    'instructions' => '',
-                    'required' => 0,
-                    'conditional_logic' => 0,
-                    'wrapper' => array(
-                        'width' => '',
-                        'class' => '',
-                        'id' => '',
-                    ),
-                ),
-            ),
-            'location' => array(
-                array(
-                    array(
-                        'param' => 'post_type',
-                        'operator' => '==',
-                        'value' => 'guide',
-                    ),
-                ),
-            ),
-            'menu_order' => 0,
-            'position' => 'side',
-            'style' => 'default',
-            'label_placement' => 'top',
-            'instruction_placement' => 'label',
-            'hide_on_screen' => '',
-            'active' => 1,
-            'description' => '',
-        ));
-
-        acf_add_local_field_group(array(
             'key' => 'group_589dcf7e047a8',
             'title' => __('Guide apperance settings', 'event-manager'),
             'fields' => array(
@@ -107,6 +61,30 @@ class AcfGuideSettings
                     'wrapper' => array(
                         'width' => '',
                         'class' => 'colorpicker',
+                        'id' => '',
+                    ),
+                ),
+                array(
+                    'return_format' => 'array',
+                    'preview_size' => 'thumbnail',
+                    'library' => 'all',
+                    'min_width' => '',
+                    'min_height' => '',
+                    'min_size' => '',
+                    'max_width' => '',
+                    'max_height' => '',
+                    'max_size' => '',
+                    'mime_types' => 'png, jpg, jpeg',
+                    'key' => 'field_58a2d24ca466c',
+                    'label' => __('Mood image', 'event-manager'),
+                    'name' => 'guide_taxonomy_image',
+                    'type' => 'image',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
                         'id' => '',
                     ),
                 ),
