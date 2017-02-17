cda43d671e1a83851da9bb426e6abd674eee358a
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-02-17 11:44:38 +0100

    Moved post modal class.

--- dist/js/hbg-event-importer.dev.js
+++ dist/js/hbg-event-importer.dev.js
@@ -3028,52 +3028,6 @@ ImportEvents.Parser.Eventhandling = (function ($) {
 })(jQuery);
 
 ImportEvents = ImportEvents || {};
-ImportEvents.Admin = ImportEvents.Admin || {};
-
-ImportEvents.Admin.NewPostModal = (function ($) {
-
-    function NewPostModal() {
-        $(function(){
-
-            this.createTrigger('location', '.acf-field-576117c423a52');
-            this.createTrigger('contact', '.acf-field-57ebb807988f8');
-            this.createTrigger('sponsor', '.acf-field-57a9d5f3804e1');
-            this.createTrigger('membership-card', '.acf-field-57c7ed92054e6');
-            this.createTrigger('membership-card', '.acf-field-581847f9642dc');
-
-            this.bindLaunchModal();
-
-        }.bind(this));
-    }
-
-    /**
-     * Create button to trigger new post modal
-     * @param  string   posttype to create
-     * @param  string   triggering class or id
-     * @return void
-     */
-    NewPostModal.prototype.createTrigger = function(postType, triggerClass) {
-        if($(triggerClass).length) {
-            $(triggerClass).append('<a class="createNewPost button" href="//' + window.location.host + '/wp/wp-admin/post-new.php?post_type=' + postType+ '&lightbox=true">' + eventmanager['new_' + postType] + '</a>');
-        }
-    };
-
-    /**
-     * Hook on trigger button to launch modal
-     * @return void
-     */
-    NewPostModal.prototype.bindLaunchModal = function() {
-        $(document).on('click','.createNewPost',function(e) {
-            e.preventDefault();
-            ImportEvents.Prompt.Modal.open($(this).attr('href'), $('#post_ID').val());
-        });
-    };
-
-    return new NewPostModal();
-
-})(jQuery);
-
-ImportEvents = ImportEvents || {};
 ImportEvents.Prompt = ImportEvents.Prompt || {};
 
 ImportEvents.Prompt.Modal = (function ($) {
@@ -3122,5 +3076,51 @@ ImportEvents.Prompt.Modal = (function ($) {
     };
 
     return new Modal();
+
+})(jQuery);
+
+ImportEvents = ImportEvents || {};
+ImportEvents.Admin = ImportEvents.Admin || {};
+
+ImportEvents.Admin.NewPostModal = (function ($) {
+
+    function NewPostModal() {
+        $(function(){
+
+            this.createTrigger('location', '.acf-field-576117c423a52');
+            this.createTrigger('contact', '.acf-field-57ebb807988f8');
+            this.createTrigger('sponsor', '.acf-field-57a9d5f3804e1');
+            this.createTrigger('membership-card', '.acf-field-57c7ed92054e6');
+            this.createTrigger('membership-card', '.acf-field-581847f9642dc');
+
+            this.bindLaunchModal();
+
+        }.bind(this));
+    }
+
+    /**
+     * Create button to trigger new post modal
+     * @param  string   posttype to create
+     * @param  string   triggering class or id
+     * @return void
+     */
+    NewPostModal.prototype.createTrigger = function(postType, triggerClass) {
+        if($(triggerClass).length) {
+            $(triggerClass).append('<a class="createNewPost button" href="//' + window.location.host + '/wp/wp-admin/post-new.php?post_type=' + postType+ '&lightbox=true">' + eventmanager['new_' + postType] + '</a>');
+        }
+    };
+
+    /**
+     * Hook on trigger button to launch modal
+     * @return void
+     */
+    NewPostModal.prototype.bindLaunchModal = function() {
+        $(document).on('click','.createNewPost',function(e) {
+            e.preventDefault();
+            ImportEvents.Prompt.Modal.open($(this).attr('href'), $('#post_ID').val());
+        });
+    };
+
+    return new NewPostModal();
 
 })(jQuery);
