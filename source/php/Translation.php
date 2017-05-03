16ff12d0996fe55138d41019308005b6b1636970
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-05-03 11:09:24 +0200

    Embedded translation configuration for guides.

---
+++ source/php/Translation.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace HbgEventImporter;
+
+class Translation
+{
+    public function __construct()
+    {
+        add_filter('wp-content-translator/configuration/meta', array($this, 'translationConfigurationGuides'));
+    }
+
+    public function translationConfigurationGuides($config)
+    {
+        array_push($config->untranslatable,
+            "guidegroup",
+            "guide_kids",
+            "guide_location",
+            "guide_beacon_namespace"
+        );
+
+        //Repeating fields
+        for ($x = 0; $x <= 100; $x++) {
+            array_push($config->translatable,
+                "guide_beacon_".$x."_location",
+                "guide_beacon_".$x."_distance",
+                "guide_beacon_".$x."_objects",
+
+                "guide_content_objects_".$x."_guide_object_title",
+                "guide_content_objects_".$x."_guide_object_id",
+                "guide_content_objects_".$x."_guide_object_description",
+                "guide_content_objects_".$x."_guide_object_image",
+                "guide_content_objects_".$x."_guide_object_audio",
+                "guide_content_objects_".$x."_guide_object_video",
+                "guide_content_objects_".$x."_guide_object_links",
+                "guide_content_objects_".$x."_guide_object_uid",
+                "guide_content_objects_".$x."_guide_object_active"
+            );
+        }
+
+        return $config;
+    }
+}
