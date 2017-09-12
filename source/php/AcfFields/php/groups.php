c79a80b9a3fe3d0a9ebcde25594469ef551fcf3e
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-09-12 10:03:21 +0200

    Removed "missing user group" option acf

--- source/php/AcfFields/php/groups.php
+++ source/php/AcfFields/php/groups.php
@@ -6,40 +6,13 @@
     'title' => __('Groups', 'event-manager'),
     'fields' => array(
         0 => array(
-            'key' => 'field_5885f58a36f81',
-            'label' => __('Missing user group', 'event-manager'),
-            'name' => 'missing_user_group',
-            'type' => 'true_false',
-            'instructions' => '',
-            'required' => 0,
-            'conditional_logic' => 0,
-            'wrapper' => array(
-                'width' => '',
-                'class' => '',
-                'id' => '',
-            ),
-            'message' => __('Check this box if the post does not belong to any user groups. This post will be available to all users.', 'event-manager'),
-            'default_value' => 0,
-            'ui' => 0,
-            'ui_on_text' => '',
-            'ui_off_text' => '',
-        ),
-        1 => array(
             'key' => 'field_5885f58c36f82',
             'label' => __('Groups', 'event-manager'),
             'name' => 'user_groups',
             'type' => 'taxonomy',
             'instructions' => __('Select one or many user groups.', 'event-manager'),
             'required' => 1,
-            'conditional_logic' => array(
-                0 => array(
-                    0 => array(
-                        'field' => 'field_5885f58a36f81',
-                        'operator' => '!=',
-                        'value' => '1',
-                    ),
-                ),
-            ),
+            'conditional_logic' => 0,
             'wrapper' => array(
                 'width' => '',
                 'class' => '',
