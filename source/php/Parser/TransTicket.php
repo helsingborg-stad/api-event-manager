d6bdcec1a6d3e8548de1eaf279d05ee772c10293
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2018-04-27 11:15:16 +0200

    new input, formated code

--- source/php/Parser/TransTicket.php
+++ source/php/Parser/TransTicket.php
@@ -23,8 +23,9 @@ class TransTicket extends \HbgEventImporter\Parser
      */
     private function getEventData()
     {
-        $url = $this->url. '&FromDate='.date("Y-m-d").'&ToDate='.date("Y-m-d", strtotime("+12 months"));
-        return json_decode( \HbgEventImporter\Helper\Curl::request('GET', $url, $this->apiKeys['transticket_api_key'], false, 'json', array('Content-Type: application/json')) );
+        $url = $this->url . '&FromDate=' . date("Y-m-d") . '&ToDate=' . date("Y-m-d", strtotime("+12 months"));
+        return json_decode(\HbgEventImporter\Helper\Curl::request('GET', $url, $this->apiKeys['transticket_api_key'],
+            false, 'json', array('Content-Type: application/json')));
     }
 
 
@@ -52,38 +53,38 @@ class TransTicket extends \HbgEventImporter\Parser
 
     /**
      * Cleans a single events data into correct format and saves it to db
-     * @param  object $eventData  Event data
-     * @param  int    $shortKey   unique key, created from the api url
+     * @param  object $eventData Event data
+     * @param  int $shortKey unique key, created from the api url
+     * @throws \Exception
      * @return void
      */
     public function saveEvent($eventData, $shortKey)
     {
 
-        $data['postTitle'] =  strip_tags (isset($eventData->Name) && !empty($eventData->Name) ? $eventData->Name : null );
-        $data['postContent'] =  strip_tags(isset($eventData->Description) && !empty($eventData->Description) ? $eventData->Description : '' , '<p><br>');
+        $data['postTitle'] = strip_tags(isset($eventData->Name) && !empty($eventData->Name) ? $eventData->Name : null);
+        $data['postContent'] = strip_tags(isset($eventData->Description) && !empty($eventData->Description) ? $eventData->Description : '',
+            '<p><br>');
 
         //$data['location'] = $this->findWordWithCapLetters($data['postContent']);
         //echo $data['location'];
 
         $data['uId'] = $eventData->Id;
         $data['event_link'] = $this->apiKeys['transticket_ticket_url'] . "/" . $data['uId'] . "/false";
-            //$data['city'] =  isset($eventData->VenueCity) && !empty($eventData->VenueCity) ? $eventData->VenueCity : null;
 
-        $data['startDate'] =  isset($eventData->EventDate) && !empty($eventData->EventDate) ? $eventData->EventDate : null ;
-        $data['endDate'] =  isset($eventData->EndDate) && !empty($eventData->EndDate) ? $eventData->EndDate : null ;
+        $data['startDate'] = isset($eventData->EventDate) && !empty($eventData->EventDate) ? $eventData->EventDate : null;
+        $data['endDate'] = isset($eventData->EndDate) && !empty($eventData->EndDate) ? $eventData->EndDate : null;
         if ($data['endDate'] === null) {
-            $data['endDate'] = $this->formatDate( date("Y-m-d H:i:s", strtotime($data['endDate'] . "+1 hour") ));
+            $data['endDate'] = $this->formatDate(date("Y-m-d H:i:s", strtotime($data['endDate'] . "+1 hour")));
         }
 
-        // Fixa
-        // $data['event_categories'] =  isset($eventData->Tags) && !empty($eventData->Tags) ? $eventData->Tags : null;
         $data['categories'] = array();
 
-        $data['postStatus'] = get_field('transticket_post_status', 'option') ? get_field('transticket_post_status', 'option') : 'publish';
-        $data['user_groups'] = (is_array($this->apiKeys['transticket_groups']) && ! empty($this->apiKeys['transticket_groups'])) ? array_map('intval', $this->apiKeys['transticket_groups']) : null;
+        $data['postStatus'] = get_field('transticket_post_status', 'option') ? get_field('transticket_post_status',
+            'option') : 'publish';
+        $data['user_groups'] = (is_array($this->apiKeys['transticket_groups']) && !empty($this->apiKeys['transticket_groups'])) ? array_map('intval',
+            $this->apiKeys['transticket_groups']) : null;
 
-        // Fixa
-        $data['ticketUrl'] = null;
+        $data['ticketUrl'] = $this->apiKeys['transticket_ticket_url'] . "/" . $data['uId'] . "/false";
 
         $data['image'] = null;
         if (isset($eventData->{'ImageURL'}) && !empty($eventData->{'ImageURL'}) && $eventData->{'ImageURL'} != 'null') {
@@ -93,6 +94,13 @@ class TransTicket extends \HbgEventImporter\Parser
         if (!is_string($data['postTitle'])) {
             return;
         }
+
+        // Various data vars not in default setup
+        $data['ticket_stock'] = isset($eventData->Stock) && !empty($eventData->Stock) ? $eventData->Stock : null;
+        $data['ticket_release_date'] = $this->formatDate(isset($eventData->ReleaseDate) && !empty($eventData->ReleaseDate) ? $eventData->ReleaseDate : null);
+        $data['tickets_remaining'] = isset($eventData->Sales['RemainingTickets']) && !empty($eventData->Sales['RemainingTickets']) ? $eventData->Sales['RemainingTickets'] : null;
+        $data['tickets_remaining'] = $this->formatDate(isset($eventData->Sales['RemainingTickets']) && !empty($eventData->Sales['RemainingTickets']) ? $eventData->Sales['RemainingTickets'] : null);
+        $data['ticket_price_range'] = isset($eventData->Sales['TicketPrices']) && !empty($eventData->Sales['TicketPrices']) ? $eventData->Sales['TicketPrices'] : null;
 
         $data['occasions'] = array();
         if ($data['startDate'] != null && $data['endDate'] != null) {
@@ -108,62 +116,85 @@ class TransTicket extends \HbgEventImporter\Parser
         $this->maybeCreateEvent($data, $shortKey, $locationId);
     }
 
+
     /**
      * Creates or updates an event if possible
-     * @param  array  $data         Event data
-     * @param  string $shortKey     Event unique short key
-     * @param  int    $locationId   Location id
+     * @param  array $data Event data
+     * @param  string $shortKey Event unique short key
+     * @param  int $locationId Location id
      * @return boolean|int          Event id or false
+     * @throws \Exception
      */
     public function maybeCreateEvent($data, $shortKey, $locationId)
     {
-        extract($data);
 
-        $eventId = $this->checkIfPostExists('event', $postTitle);
+        $eventId = $this->checkIfPostExists('event', $data['postTitle']);
         $occurred = false;
 
-        $eventManagerUid = (get_post_meta($eventId, '_event_manager_uid', true)) ? get_post_meta($eventId, '_event_manager_uid', true) : 'transticket-' . $shortKey . '-' . $uId;
-
+        $eventManagerUid = (get_post_meta($eventId, '_event_manager_uid', true)) ? get_post_meta($eventId,
+            '_event_manager_uid', true) : 'transticket-' . $shortKey . '-' . $data['uId'];
+        $postStatus = $data['postStatus'];
         // Get existing event meta data
         if ($eventId) {
-            $sync           = get_post_meta($eventId, 'sync', true);
-            $postStatus     = get_post_status($eventId);
+            $sync = get_post_meta($eventId, 'sync', true);
+            $postStatus = get_post_status($eventId);
             $levenshteinKey = array_search($eventId, array_column($this->levenshteinTitles['event'], 'ID'));
-            $occurred       = $this->levenshteinTitles['event'][$levenshteinKey]['occurred'];
+            $occurred = $this->levenshteinTitles['event'][$levenshteinKey]['occurred'];
         }
 
-        if (($eventId && !$sync) || !$this->filter($categories)) {
+        if (($eventId && !$sync) || !$this->filter($data['categories'])) {
             return $eventId;
         }
 
-        $event = new Event(
-            array(
-                'post_title'              => $postTitle,
-                'post_content'            => $postContent,
-                'post_status'             => $postStatus,
-            ),
-            array(
-                '_event_manager_uid'      => $eventManagerUid,
-                'sync'                    => 1,
-                'status'                  => 'Active',
-                'image'                   => !empty($image) ? $image : null,
-                'event_link'              => null,
-                'categories'              => $categories,
-                'occasions'               => $occasions,
-                'location'                => $locationId != null ? $locationId : null,
-                'organizer'              => null,
-                'booking_link'            => is_string($ticketUrl) ? $ticketUrl : null,
-                'booking_phone'           => null,
-                'age_restriction'         => null,
-                'price_information'       => null,
-                'price_adult'             => null,
-                'price_children'          => null,
-                'import_client'           => 'transticket',
-                'imported_post'           => 1,
-                'user_groups'             => $user_groups,
-                'occurred'                => $occurred,
-            )
-        );
+        $categories = $this->getCategories($data['event_categories']);
+
+        if (!empty($data['ticket_price_range'])) {
+            $data['ticket_price_range'] = array(
+                "seated_minimum_price" => $data['ticket_price_range']['SeatedMinPrice'],
+                "seated_maximum_price" => $data['ticket_price_range']['SeatedMaxPrice'],
+                "standing_minimum_price" => $data['ticket_price_range']['StandingMinPrice'],
+                "standing_maximum_price" => $data['ticket_price_range']['StandingMaxPrice'],
+            );
+        }
+
+        try {
+            $event = new Event(
+                array(
+                    'post_title' => $data['postTitle'],
+                    'post_content' => $data['postContent'],
+                    'post_status' => $postStatus,
+                ),
+                array(
+                    '_event_manager_uid' => $eventManagerUid,
+                    'sync' => 1,
+                    'status' => 'Active',
+                    'image' => !empty($data['image']) ? $data['image'] : null,
+                    'event_link' => !empty($data['ticketUrl']) ? $data['ticketUrl'] : null,
+                    'categories' => $categories,
+                    'occasions' => $data['occasions'],
+                    'location' => $locationId != null ? $locationId : null,
+                    'organizer' => null,
+                    'booking_link' => is_string($data['ticketUrl']) ? $data['ticketUrl'] : null,
+                    'booking_phone' => null,
+                    'age_restriction' => null,
+                    'price_information' => null,
+                    'price_adult' => null,
+                    'price_children' => null,
+                    'import_client' => 'transticket',
+                    'imported_post' => 1,
+                    'user_groups' => $data['user_groups'],
+                    'occurred' => $occurred,
+                    'ticket_stock' => $data['ticket_stock'],
+                    'ticket_release_date' => $data['ticket_release_date'],
+                    'tickets_remaining' => $data['tickets_remaining'],
+                    'ticket_price_range' => $data['ticket_price_range'],
+
+                )
+            );
+        } catch (\Exception $e) {
+            $event = false;
+            error_log($e);
+        }
 
         if (!$event->save()) {
             return false;
@@ -172,9 +203,9 @@ class TransTicket extends \HbgEventImporter\Parser
         if (!$eventId) {
             $this->nrOfNewEvents++;
             $this->levenshteinTitles['event'][] = array(
-                'ID'         => $event->ID,
-                'post_title' => $postTitle,
-                'occurred'   => true,
+                'ID' => $event->ID,
+                'post_title' => $data['postTitle'],
+                'occurred' => true,
             );
         } else {
             $this->levenshteinTitles['event'][$levenshteinKey]['occurred'] = true;
@@ -189,21 +220,20 @@ class TransTicket extends \HbgEventImporter\Parser
 
     /**
      * Creates or updates a location if possible
-     * @param  array $data  Event data
+     * @param  array $data Event data
      * @return boolean|int  Location id or false
+     * @throws \Exception
      */
     public function maybeCreateLocation($data, $shortKey)
     {
-        extract($data);
-
-        if (!is_string($address)) {
+        if (!is_string($data['address'])) {
             return false;
         }
 
         // Checking if there is a location already with this title or similar enough
-        $locationId = $this->checkIfPostExists('location', $address);
+        $locationId = $this->checkIfPostExists('location', $data['address']);
 
-        $locPostStatus = $postStatus;
+        $locPostStatus = $data['postStatus'];
         $isUpdate = false;
 
         // Check if this is a duplicate or update and if "sync" option is set.
@@ -212,7 +242,7 @@ class TransTicket extends \HbgEventImporter\Parser
             $sync = get_post_meta($locationId, 'sync', true);
             $locPostStatus = get_post_status($locationId);
 
-            if ($existingUid == 'transticket-' . $shortKey . '-' . $this->cleanString($address) && $sync == 1) {
+            if ($existingUid == 'transticket-' . $shortKey . '-' . $this->cleanString($data['address']) && $sync == 1) {
                 $isUpdate = true;
             }
         }
@@ -222,36 +252,41 @@ class TransTicket extends \HbgEventImporter\Parser
         }
 
         // Create the location
-        $location = new Location(
-            array(
-                'post_title'            => $address,
-                'post_status'           => $locPostStatus,
-            ),
-            array(
-                'street_address'        => null,
-                'postal_code'           => null,
-                'city'                  => $city,
-                'municipality'          => null,
-                'country'               => null,
-                'latitude'              => null,
-                'longitude'             => null,
-                'import_client'         => 'transticket',
-                '_event_manager_uid'    => 'transticket-' . $shortKey . '-' . $this->cleanString($address),
-                'user_groups'           => $user_groups,
-                'sync'                  => 1,
-                'imported_post'         => 1,
-            )
-        );
+        try {
+            $location = new Location(
+                array(
+                    'post_title' => $data['address'],
+                    'post_status' => $locPostStatus,
+                ),
+                array(
+                    'street_address' => null,
+                    'postal_code' => null,
+                    'city' => $data['city'],
+                    'municipality' => null,
+                    'country' => null,
+                    'latitude' => null,
+                    'longitude' => null,
+                    'import_client' => 'transticket',
+                    '_event_manager_uid' => 'transticket-' . $shortKey . '-' . $this->cleanString($data['address']),
+                    'user_groups' => $data['user_groups'],
+                    'sync' => 1,
+                    'imported_post' => 1,
+                )
+            );
+        } catch (\Exception $e) {
+            $location = false;
+            error_log($e);
+        }
 
         if (!$location->save()) {
             return false;
         }
 
-        if (! isset($location->duplicate)) {
+        if (!isset($location->duplicate)) {
             $this->nrOfNewLocations++;
         }
 
-        $this->levenshteinTitles['location'][] = array('ID' => $location->ID, 'post_title' => $address);
+        $this->levenshteinTitles['location'][] = array('ID' => $location->ID, 'post_title' => $data['address']);
 
         return $location->ID;
     }
@@ -266,7 +301,7 @@ class TransTicket extends \HbgEventImporter\Parser
         $passes = true;
         $exclude = $this->apiKeys['transticket_filter_tags'];
 
-        if (! empty($exclude)) {
+        if (!empty($exclude)) {
             $filters = array_map('trim', explode(',', $exclude));
             $categoriesLower = array_map('strtolower', $categories);
 
@@ -292,7 +327,8 @@ class TransTicket extends \HbgEventImporter\Parser
         }
         // Format the date string corretly
         $dateParts = explode("T", $date);
-        $dateString = substr($dateParts[0], 0, 4) . '-' . substr($dateParts[0], 4, 2) . '-' . substr($dateParts[0], 6, 2);
+        $dateString = substr($dateParts[0], 0, 4) . '-' . substr($dateParts[0], 4, 2) . '-' . substr($dateParts[0], 6,
+                2);
         $timeString = substr($dateParts[1], 0, 4);
         $timeString = substr($timeString, 0, 2) . ':' . substr($timeString, 2, 2);
         $dateString = $dateString . ' ' . $timeString;
@@ -310,6 +346,31 @@ class TransTicket extends \HbgEventImporter\Parser
         return str_replace(' ', 'T', $returnDate->format('Y-m-d H:i:s'));
     }
 
+
+    /**
+     * Get categories from event data
+     * @param  object $eventData Event data object
+     * @return array             Categories
+     */
+    public function getCategories($eventData)
+    {
+        $categories = array();
+
+        if (is_array($eventData->Categories->Category)) {
+            foreach ($eventData->Categories->Category as $category) {
+                $categories[] = $category->Name;
+            }
+        } else {
+            $categories[] = $eventData->Categories->Category->Name;
+        }
+
+        $categories = array_map('trim', $categories);
+        $categories = array_map('ucwords', $categories);
+
+        return $categories;
+    }
+
+
     /**
      * Washing string from capitalized words. Add capital letter to first word in sentence.
      * @param  string
@@ -326,8 +387,9 @@ class TransTicket extends \HbgEventImporter\Parser
      * @param  string
      * @return string
      */
-    public function findWordWithCapLetters($str) {
-        if (preg_match_all('/\\b(?=[A-Z])[A-Z ]+(?=\\W)/',$str,$match)) {
+    public function findWordWithCapLetters($str)
+    {
+        if (preg_match_all('/\\b(?=[A-Z])[A-Z ]+(?=\\W)/', $str, $match)) {
             return $match[0];
         }
     }
