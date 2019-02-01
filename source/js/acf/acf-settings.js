f3111db317db1aa3c8e115af1f515ed07cf39c33
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-02-01 13:03:34 +0100

    Updated validation filter to follow Acfs new guidelines <5.7

--- source/js/acf/acf-settings.js
+++ source/js/acf/acf-settings.js
@@ -1,57 +1,54 @@
-// ACF date picker settings
 (function($) {
-    if (typeof acf !== 'undefined') {
-        // Datepicker translations
-        acf.add_filter('date_time_picker_args', function(args, $field) {
-            args.timeOnlyTitle = eventmanager.choose_time;
-            args.timeText = eventmanager.time;
-            args.hourText = eventmanager.hour;
-            args.minuteText = eventmanager.minute;
-            args.closeText = eventmanager.done;
-            args.currentText = eventmanager.now;
-            args.showSecond = false;
-            return args;
-        });
+    // Datepicker translations
+    acf.add_filter('date_time_picker_args', function(args, $field) {
+        args.timeOnlyTitle = eventmanager.choose_time;
+        args.timeText = eventmanager.time;
+        args.hourText = eventmanager.hour;
+        args.minuteText = eventmanager.minute;
+        args.closeText = eventmanager.done;
+        args.currentText = eventmanager.now;
+        args.showSecond = false;
+        return args;
+    });
 
-        acf.add_filter('time_picker_args', function(args, $field) {
-            args.timeOnlyTitle = eventmanager.choose_time;
-            args.timeText = eventmanager.time;
-            args.hourText = eventmanager.hour;
-            args.minuteText = eventmanager.minute;
-            args.closeText = eventmanager.done;
-            args.currentText = eventmanager.now;
-            args.showSecond = false;
-            return args;
-        });
+    acf.add_filter('time_picker_args', function(args, $field) {
+        args.timeOnlyTitle = eventmanager.choose_time;
+        args.timeText = eventmanager.time;
+        args.hourText = eventmanager.hour;
+        args.minuteText = eventmanager.minute;
+        args.closeText = eventmanager.done;
+        args.currentText = eventmanager.now;
+        args.showSecond = false;
+        return args;
+    });
 
-        acf.add_filter('google_map_marker_args', function(args, $field) {
-            args.draggable = false;
-            args.raiseOnDrag = false;
-            return args;
-        });
+    acf.add_filter('google_map_marker_args', function(args, $field) {
+        args.draggable = false;
+        args.raiseOnDrag = false;
+        return args;
+    });
 
-        // Show validation errors on tabs
-        acf.add_filter('validation_complete', function(json, $form) {
-            $('.acf-tab-error', $form).remove();
+    // Show validation errors on tabs
+    acf.addFilter('validation_complete', function(args, field) {
+        $('.acf-tab-error', field).remove();
 
-            if (json.errors) {
-                for (var i = 0; i < json.errors.length; i++) {
-                    var field = $('[name="' + json.errors[i].input + '"]', $form).parents(
-                        '.acf-field'
-                    );
-                    field = field[field.length - 1];
-                    var tab = $(field, $form)
-                        .prevAll('.acf-field-tab')
-                        .attr('data-key');
+        if (args.errors) {
+            for (var i = 0; i < args.errors.length; i++) {
+                var errorField = $('[name="' + args.errors[i].input + '"]', field).parents(
+                    '.acf-field'
+                );
+                errorField = errorField[errorField.length - 1];
+                var tab = $(errorField, field)
+                    .prevAll('.acf-field-tab')
+                    .attr('data-key');
 
-                    $('.acf-tab-wrap a[data-key=' + tab + '] .acf-tab-error', $form).remove();
-                    $('.acf-tab-wrap a[data-key=' + tab + ']', $form)
-                        .append(' <span class="dashicons dashicons-warning acf-tab-error"></span>')
-                        .click();
-                }
+                $('.acf-tab-wrap a[data-key=' + tab + '] .acf-tab-error', field).remove();
+                $('.acf-tab-wrap a[data-key=' + tab + ']', field)
+                    .append(' <span class="dashicons dashicons-warning acf-tab-error"></span>')
+                    .click();
             }
+        }
 
-            return json;
-        });
-    }
+        return args;
+    });
 })(jQuery);
