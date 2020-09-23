2f24e3024989b025da3c25433ce22cd66fb4af6c
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-09-23 11:58:33 +0200

    Ads a better field name for step type

--- source/php/Api/InteractiveGuideFields.php
+++ source/php/Api/InteractiveGuideFields.php
@@ -29,12 +29,38 @@ class InteractiveGuideFields extends Fields
             $this->postType,
             'steps',
             array(
-                'get_callback' => array($this, 'objectGetCallBack'),
+                'get_callback' => array($this, 'interactiveGuideStepsGetCallBack'),
                 'schema' => array(
                     'description' => 'Field containing object value with interactive guide steps.',
                     'type' => 'string',
                 )
             )
         );
+    }
+
+
+    /**
+     * Ads a better field name for step type
+     *
+     * @param   object  $object      The response object.
+     * @param   string  $field_name  The name of the field to add.
+     * @param   object  $request     The WP_REST_Request object.
+     *
+     * @return  object|null
+     */
+    public function interactiveGuideStepsGetCallBack($object, $fieldName, $request)
+    {
+        $stepMetaData = $this->objectGetCallBack($object, $fieldName, $request);
+        if (!is_array($stepMetaData) || empty($stepMetaData)) {
+            return $stepMetaData;
+        }
+
+        foreach ($stepMetaData as &$step) {
+            $type = isset($step['acf_fc_layout']) ? array('type' => $step['acf_fc_layout']) : array('type' => null);
+            $step = array_merge($type, $step);
+            unset($step['acf_fc_layout']);
+        }
+
+        return $stepMetaData;
     }
 }
