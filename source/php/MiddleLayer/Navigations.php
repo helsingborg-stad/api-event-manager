d79e37da4c7b3e970b76a804bc7c49ab76118370
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-11-19 15:12:16 +0100

    fixed navigation delete

--- source/php/MiddleLayer/Navigations.php
+++ source/php/MiddleLayer/Navigations.php
@@ -12,9 +12,26 @@ class Navigations extends \HbgEventImporter\MiddleLayer\SyncManager
         parent::__construct($this->singularName, $this->pluralName);
 
         if ($this->isCdnSyncEnabled) {
-            add_action('create_' . $this->singularName, array($this, 'saveItem'), 10, 2);
-            add_action('edited_' . $this->singularName, array($this, 'saveItem'), 10, 2);
-            add_action('delete_' . $this->singularName, array($this, 'deleteItem'), 10, 1);
+            add_action('create_' . $this->singularName, array($this, 'saveItem'), 10);
+            add_action('edited_' . $this->singularName, array($this, 'saveItem'), 10);
+            add_action('pre_delete_term', array($this, 'deleteNavigation'), 10, 2);
         }
+    }
+
+    public function deleteNavigation($termId, $taxonomy)
+    {
+        if ($taxonomy !== $this->singularName) {
+            return;
+        }
+
+        $data = $this->getRestResponse($termId);
+        $groupId = $data['user_groups']['id'] ?? null;
+        $lang = $data['lang'] ?? null;
+        if (empty($data) || empty($groupId) || empty($lang)) {
+            return;
+        }
+
+        $path = sprintf('%s/%s/%s', $groupId, $lang, $termId);
+        $this->deleteItem($path);
     }
 }
