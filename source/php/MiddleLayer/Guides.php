6aea501d8be0dab73956489c74cf45341c0f1041
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-11-22 11:38:51 +0100

    fixed get post type bug

--- source/php/MiddleLayer/Guides.php
+++ source/php/MiddleLayer/Guides.php
@@ -13,7 +13,7 @@ class Guides extends \HbgEventImporter\MiddleLayer\SyncManager
 
         if ($this->isCdnSyncEnabled) {
             add_action('save_post_' . $this->singularName, array($this, 'saveGuide'), 10, 3);
-            add_action('delete_post', array($this, 'deleteGuide'), 10, 2);
+            add_action('delete_post', array($this, 'deleteGuide'), 10);
         }
     }
 
@@ -31,9 +31,10 @@ class Guides extends \HbgEventImporter\MiddleLayer\SyncManager
         $this->deleteItem($postId);
     }
 
-    public function deleteGuide($postId, $post)
+    public function deleteGuide($postId)
     {
-        if ($post->post_type !== $this->singularName) {
+        $post = get_post($postId);
+        if (empty($post) || $post->post_type !== $this->singularName) {
             return;
         }
 
