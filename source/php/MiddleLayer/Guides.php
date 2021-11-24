9139d7d86b3ce9765e82d710f3a9e31f3add5ec6
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-11-24 13:27:41 +0100

    populate guides cron event

--- source/php/MiddleLayer/Guides.php
+++ source/php/MiddleLayer/Guides.php
@@ -40,4 +40,34 @@ class Guides extends \HbgEventImporter\MiddleLayer\SyncManager
 
         $this->deleteItem($postId);
     }
+
+    public function getGuides($page = 1, $items = [])
+    {
+        $args = array(
+                'paged' => $page,
+                'posts_per_page' => 50,
+                'post_type'   => $this->singularName,
+                'post_status' => 'publish',
+            );
+        $query = new \WP_Query($args);
+
+        if (!empty($query->posts)) {
+            $items = array_merge($items, $query->posts);
+        }
+
+        if ($query->max_num_pages && (int)$page < (int)$query->max_num_pages) {
+            return $this->getGuides(++$page, $items);
+        }
+
+        return $items;
+    }
+
+    public function startPopulate()
+    {
+        $posts = $this->getGuides();
+
+        foreach ($posts as $key => $post) {
+            $this->saveEmbeddedItem($post->ID);
+        }
+    }
 }
