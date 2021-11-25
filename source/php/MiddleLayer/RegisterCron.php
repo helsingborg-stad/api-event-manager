ff550e2b728f5d871d9d484817d102926fc0a8cf
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-11-25 09:28:57 +0100

    trigger populate cron with ajax call

--- source/php/MiddleLayer/RegisterCron.php
+++ source/php/MiddleLayer/RegisterCron.php
@@ -6,12 +6,7 @@ class RegisterCron
 {
     public function __construct()
     {
-        add_action('admin_init', function () {
-            if (isset($_GET['populatemiddlelayer'])) {
-                $this->schedulePopulate();
-            }
-        }, 10);
-
+        add_action('wp_ajax_schedule_populate_middle_layer', array($this, 'schedulePopulate'));
         add_action('populate_middle_layer_api', array($this, 'populateMiddleLayerApi'), 10, 1);
     }
 
@@ -19,8 +14,10 @@ class RegisterCron
     {
         $classes = ['Languages', 'Navigations', 'GuideGroups', 'Guides'];
         foreach ($classes as $class) {
+            wp_clear_scheduled_hook('populate_middle_layer_api', array('class' => $class));
             wp_schedule_single_event(time(), 'populate_middle_layer_api', array('class' => $class));
         }
+        wp_die();
     }
 
     public function populateMiddleLayerApi($className)
