cc4eac2fbdfac7293e45f48ac4f9d58e60d0d242
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-03-11 14:55:45 +0100

    fix: detect and hide fields
    Hidden fields generates an offset on bool error at this stage.

--- source/php/AcfFields/php/event-fields.php
+++ source/php/AcfFields/php/event-fields.php
@@ -67,6 +67,7 @@
                 'class' => '',
                 'id' => '',
             ),
+            'is_publicly_hidden' => 1,
             'default_value' => '',
             'maxlength' => '',
             'rows' => '',
--- source/php/FieldSettingHidePublic.php
+++ source/php/FieldSettingHidePublic.php
@@ -17,15 +17,31 @@ class FieldSettingHidePublic implements Hookable
     public function addHooks(): void
     {
         $this->wpService->addAction('acf/render_field_settings', [$this, 'addPublicFieldOption']);
+        $this->wpService->addAction('acf/prepare_field', [$this, 'hideFieldFromFrontendForms']);
     }
 
     public function addPublicFieldOption($field) {
-        acf_render_field_setting( $field, array(
-            'label'        => __('Display in frontend forms', 'api-event-manager'),
+        acf_render_field_setting($field, array(
+            'label'        => __('Hide in frontend forms', 'api-event-manager'),
             'instructions' => 'Wheter to display this field in frontend forms or not.',
-            'name'         => 'is_public',
+            'name'         => 'is_publicly_hidden',
             'type'         => 'true_false',
             'ui'           => 1,
         ), true );
+    }
+
+    public function hideFieldFromFrontendForms($field) {
+        
+        //Set default
+        if (!isset($field['is_publicly_hidden'])) {
+            $field['is_publicly_hidden'] = 0;
+        }
+
+        //Hide field from frontend forms
+        if ($field['is_publicly_hidden'] == 1) {
+            return false;
+        }
+
+        return $field;
     }
 }
