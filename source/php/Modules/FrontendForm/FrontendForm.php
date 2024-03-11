71a208c56f958fc2417c95fbffa496b1b44453a6
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-03-11 11:14:16 +0100

    feat: allow editors to filter the field groups displayed.

--- source/php/Modules/FrontendForm/FrontendForm.php
+++ source/php/Modules/FrontendForm/FrontendForm.php
@@ -25,21 +25,30 @@ class FrontendForm extends \Modularity\Module
         $this->namePlural   = __('Event Forms', 'api-event-manager');
         $this->description  = __('Module for creating public event form', 'api-event-manager');
         //$this->cacheTtl     = 60;
+
+        add_filter('acf/load_field/name=display_field_groups', [$this, 'filterFieldOptions']);
     }
 
     public function data(): array
     {
       $fields = $this->getFields();
 
+      // Filter out the keys from the field groups
+      $displayFieldGroupsKeys = array_map(function($field) {
+        return $field['value'];
+      }, $fields['display_field_groups'] ?? []);
+
+      // Return the data
       return [
         'formStart' => function() {
           
         },
-        'form' => function() {
+        'form' => function() use ($displayFieldGroupsKeys) {
           acf_form([
             'post_id' => 'new_post',
             'post_title' => true,
             'post_content' => true,
+            'field_groups' => $displayFieldGroupsKeys,
             'new_post' => [
               'post_type' => 'event',
               'post_status' => 'draft'
@@ -55,7 +64,7 @@ class FrontendForm extends \Modularity\Module
 
     public function template(): string
     {
-        return 'frontend-form.blade.php';
+      return 'frontend-form.blade.php';
     }
 
     public function script(): void
@@ -72,5 +81,44 @@ class FrontendForm extends \Modularity\Module
             'frontend-form',
             EVENT_MANAGER_URL . '/dist/'. CacheBust::name('js/assignment-form.css')
         );*/ 
+    }
+
+    /**
+     * Filter the field options.
+     *
+     * @param array $field The field options.
+     * @return array The filtered field options.
+     */
+    public function filterFieldOptions($field): array
+    {
+      $field['choices'] = array();
+
+      $selectableFieldGroups = $this->getSelectableFieldGroups();
+      if(is_countable($selectableFieldGroups)) {
+        foreach($selectableFieldGroups as $group) {
+          $field['choices'][$group['value']] = $group['label'];
+        }
+      }
+
+      return $field;
+    }
+
+    /**
+     * Retrieves an array of selectable field groups for the 'event' post type.
+     *
+     * @return array The array of selectable field groups, where each group is represented by an associative array with 'label' and 'value' keys.
+     */
+    private function getSelectableFieldGroups(): array
+    {
+      $fieldGroups = acf_get_field_groups(array('post_type' => 'event'));
+
+      $fieldGroups = array_map(function($fieldGroup) {
+        return [
+          'label' => $fieldGroup['title'],
+          'value' => $fieldGroup['key']
+        ];
+      }, $fieldGroups);
+
+      return $fieldGroups;
     }
 }
