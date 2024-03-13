7243b339efd0c4160c8ecdeeacea5d6484825119
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-03-13 10:03:55 +0100

    feat: add enqueueScript interface

---
+++ source/php/Services/WPService/EnqueueScript.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace EventManager\Services\WPService;
+
+interface EnqueueScript
+{
+  public function enqueueScript(string $handle, string $src = '', array $deps = array(), string|bool|null $ver = false, array|bool $args = array()): void;
+}
