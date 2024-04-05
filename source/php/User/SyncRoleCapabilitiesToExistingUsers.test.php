0861fce0e50916fd1be1d09742ac2e34a1be13f4
Thor Brink <thor.brink@helsingborg.se>
2024-04-05 06:41:32 +0000

    feat: synchronize user role capabilities to existing users

---
+++ source/php/User/SyncRoleCapabilitiesToExistingUsers.test.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace EventManager\User;
+
+use EventManager\Services\WPService\AddAction;
+use EventManager\Services\WPService\GetUsers;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+use WP_User;
+
+class SyncRoleCapabilitiesToExistingUsersTest extends TestCase
+{
+    /**
+     * @testdox Synchronize role capabilities to existing users of that role
+     */
+    public function testSynchronizeRoleCapabilitiesToExistingUsersOfThatRole()
+    {
+        $capabilities                        = ['testCapability'];
+        $role                                = new Role('testRole', 'Test Role', $capabilities);
+        $user                                = $this->getUser();
+        $wpService                           = $this->getWpService([$user]);
+        $syncRoleCapabilitiesToExistingUsers = new SyncRoleCapabilitiesToExistingUsers([$role], $wpService);
+
+        $user->expects($this->once())->method('add_cap')->with('testCapability');
+
+        $syncRoleCapabilitiesToExistingUsers->sync();
+    }
+
+    private function getUser(): WP_User|MockObject
+    {
+        return $this->getMockBuilder(stdClass::class)->addMethods(['add_cap'])->getMock();
+    }
+
+    private function getWpService($users = []): GetUsers&AddAction
+    {
+        return new class ($users) implements GetUsers, AddAction {
+            public function __construct(private array $users)
+            {
+            }
+
+            public function addAction(
+                string $tag,
+                callable $function_to_add,
+                int $priority = 10,
+                int $accepted_args = 1
+            ): bool {
+                return true;
+            }
+
+            public function getUsers(array $args): array
+            {
+                return $this->users;
+            }
+        };
+    }
+}
