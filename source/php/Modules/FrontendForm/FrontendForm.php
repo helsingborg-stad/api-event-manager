e8da2f8b52422341a334125a63209e0847376e97
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-04-16 11:53:20 +0200

    fix: listen to step key in url, switch form partial (group)

--- source/php/Modules/FrontendForm/FrontendForm.php
+++ source/php/Modules/FrontendForm/FrontendForm.php
@@ -36,6 +36,8 @@ class FrontendForm extends \Modularity\Module
         'group_65a115157a046'
     ];
 
+    private $formStepKey = 'step'; // The query parameter for the form steps.
+
     private $blade = null;
 
     private EnqueueStyle $wpService;
@@ -48,7 +50,7 @@ class FrontendForm extends \Modularity\Module
 
         $this->wpService = new NativeWpService(); // TODO: use custom modularity middleware.
 
-        
+        add_filter('query_vars',[$this, 'registerFormStepQueryVar']); // add from wpservice
     }
 
     public function data(): array
@@ -56,34 +58,39 @@ class FrontendForm extends \Modularity\Module
         $fields = $this->getFields(); //Needs to be called, otherwise a notice will be thrown.
 
         $htmlUpdatedMessage = $this->renderView('partials.message', [
-        'text' => '%s',
-        'icon' => ['name' => 'info'],
-        'type' => 'warning'
+            'text' => '%s',
+            'icon' => ['name' => 'info'],
+            'type' => 'warning'
         ]);
 
         $htmlSubmitButton = $this->renderView('partials.submit', [
-        'text' => __('Create Event', 'api-event-manager')
+            'text' => __('Create Event', 'api-event-manager')
         ]);
 
-      // Return the data
+        $currentStep = $this->getFieldGroup($this->formStepKey);
+
+        var_dump($currentStep);
+
         return [
-        'form' => function () use ($htmlUpdatedMessage, $htmlSubmitButton) {
+        'form' => function () use ($htmlUpdatedMessage, $htmlSubmitButton, $currentStep) {
             acf_form([
-            'post_id'               => 'new_post',
-            'post_title'            => true,
-            'post_content'          => false,
-            'field_groups'          => $this->fieldGroups,
-            'form_attributes' => ['class' => 'acf-form js-form-validation js-form-validation'],
-            'uploader'              => 'basic',
-            'updated_message'       => __("The event has been submitted for review. You will be notified when the event has been published.", 'acf'),
-            'html_updated_message'  => $htmlUpdatedMessage,
-            'html_submit_button'    => $htmlSubmitButton,
-            'new_post'              => [
-              'post_type'   => 'event',
-              'post_status' => 'draft'
-            ],
-            'instruction_placement' => 'field',
-            'submit_value'          => __('Create Event', 'api-event-manager')
+                'post_id'               => 'new_post',
+                'post_title'            => true,
+                'post_content'          => false,
+                'field_groups'          => [
+                    $currentStep
+                ],
+                'form_attributes' => ['class' => 'acf-form js-form-validation js-form-validation'],
+                'uploader'              => 'basic',
+                'updated_message'       => __("The event has been submitted for review. You will be notified when the event has been published.", 'acf'),
+                'html_updated_message'  => $htmlUpdatedMessage,
+                'html_submit_button'    => $htmlSubmitButton,
+                'new_post'              => [
+                'post_type'   => 'event',
+                'post_status' => 'draft'
+                ],
+                'instruction_placement' => 'field',
+                'submit_value'          => __('Create Event', 'api-event-manager')
             ]);
         }
         ];
@@ -97,7 +104,6 @@ class FrontendForm extends \Modularity\Module
     public function script(): void
     {
         acf_form_head();
-        
     }
 
     public function style(): void
@@ -132,5 +138,65 @@ class FrontendForm extends \Modularity\Module
         }
 
         return false;
+    }
+
+    /**
+     * Registers the form step query variable.
+     *
+     * This method takes an array of registered query variables and adds the form step key to it.
+     *
+     * @param array $registeredQueryVars The array of registered query variables.
+     * @return array The updated array of registered query variables.
+     */
+    public function registerFormStepQueryVar(array $registeredQueryVars): array {
+        return $registeredQueryVars = array_merge(
+            $registeredQueryVars,
+            [$this->formStepKey]
+        );
+    }
+
+    /**
+     * Retrieves the field group for a given form step.
+     *
+     * @param string $stepkey The key of the form step.
+     * @return string|bool The field group for the specified form step, or false if not found.
+     */
+    private function getFieldGroup(string $stepkey): string|bool {
+        $formStep = $this->getCurrentFormStep($stepkey);
+        $formStep = $formStep - 1;
+
+        if($formStep > count($this->fieldGroups) - 1) {
+            return false;
+        }
+
+        if(!isset($this->fieldGroups[$formStep])) {
+            return false;
+        }
+
+        return $this->fieldGroups[$formStep];    
+    }
+
+    /**
+     * Retrieves the current step of the form based on the provided step key.
+     *
+     * @param string $stepkey The key used to retrieve the current step.
+     * @return int The current step of the form.
+     */
+    private function getCurrentFormStep(string $stepkey): int {
+        $step = get_query_var($stepkey, 1);
+        if(is_numeric($step)) {
+            return $step;
+        }
+        return 1;
+    }
+
+    /**
+     * Toggles the edit mode based on the given step value.
+     *
+     * @param int $step The step value to determine the edit mode.
+     * @return string The edit mode, either 'edit' or 'create'.
+     */
+    private function toggleEditMode(int $step): string {
+        return empty($step) ? 'edit' : 'create';
     }
 }
