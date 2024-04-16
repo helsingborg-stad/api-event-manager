885992d2e0a68814fec1a933306b11bdef5e4d67
Thor Brink <thor.brink@helsingborg.se>
2024-04-16 09:38:14 +0200

    fix: set isAccessibleForFree from pricesList (#83)
    * fix: set isAccessibleForFree from pricesList
    
    * fix: check if prices are above 0 for paid events
    
    * refactor: apply coding standards

--- source/php/PostToSchema/PostToEventSchema/Commands/SetIsAccessibleForFree.php
+++ source/php/PostToSchema/PostToEventSchema/Commands/SetIsAccessibleForFree.php
@@ -6,12 +6,24 @@ use Spatie\SchemaOrg\BaseType;
 
 class SetIsAccessibleForFree implements CommandInterface
 {
+    private const META_KEY = 'pricesList';
+
     public function __construct(private BaseType $schema, private array $meta)
     {
     }
 
     public function execute(): void
     {
-        $this->schema->isAccessibleForFree((bool) $this->meta['isAccessibleForFree'] ?: null);
+        if (!isset($this->meta[self::META_KEY]) || empty($this->meta[self::META_KEY])) {
+            $this->schema->isAccessibleForFree(true);
+            return;
+        }
+
+        $listWithPricesHigherThanZero = array_filter(
+            $this->meta[self::META_KEY],
+            fn($priceRow) => (int)$priceRow['price'] > 0
+        );
+
+        $this->schema->isAccessibleForFree(empty($listWithPricesHigherThanZero));
     }
 }
