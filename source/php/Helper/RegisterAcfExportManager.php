2c5a73f90e633a78f081b583228960c955767b3c
Thor Brink <thor.brink@helsingborg.se>
2026-01-21 12:42:57 +0000

    fix: split acf fields for event prices into separate group

--- source/php/Helper/RegisterAcfExportManager.php
+++ source/php/Helper/RegisterAcfExportManager.php
@@ -28,6 +28,7 @@ class RegisterAcfExportManager implements Hookable
             'event-presentation-fields' => 'group_68d28e8bc6408',
             'event-time-fields'         => 'group_68d28f0c82ff4',
             'event-place-fields'        => 'group_696e24e564997',
+            'event-prices-fields'       => 'group_6970c90e6e6ba',
             'event-audience-category'   => 'group_68d28f55ef8cf',
             'organization-fields'       => 'group_65a4f5a847d62',
             'plugin-settings'           => 'group_660cec468b833',
