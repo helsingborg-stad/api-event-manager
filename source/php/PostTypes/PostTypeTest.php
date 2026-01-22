89747203d82cc91a9b2d56256cb24fc9f933e82a
Thor Brink <thor.brink@helsingborg.se>
2026-01-22 13:24:25 +0000

    test: increase coverage for EventManager\PostTypes\PostType

---
+++ source/php/PostTypes/PostTypeTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace EventManager\PostTypes;
+
+use EventManager\HooksRegistrar\Hookable;
+use PHPUnit\Framework\TestCase;
+use WP_Post_Type;
+use WP_Error;
+use WpService\Contracts\__;
+use WpService\Contracts\AddAction;
+use WpService\Contracts\RegisterPostType;
+
+class PostTypeTest extends TestCase {
+    /**
+     * @testdox registers posttype on init hook
+     */
+    public function test_registers_posttype_on_init_hook(): void {
+        $wpService = static::createWpService();
+        $postType = new class($wpService) extends PostType {
+            public function getName(): string {
+                return 'test_post_type';
+            }
+
+            public function getArgs(): array {
+                return ['public' => true];
+            }
+
+            public function getLabelSingular(): string {
+                return 'Test Post';
+            }
+
+            public function getLabelPlural(): string {
+                return 'Test Posts';
+            }
+        };
+        
+        $postType->addHooks();
+        $wpService->addedActions['init'][0][1]();
+
+        // Check that post type was registered 
+        static::assertArrayHasKey('test_post_type', $wpService->registeredPostTypes);
+    }
+
+    private static function createWpService(): AddAction|RegisterPostType|__ {
+        return new class implements AddAction, RegisterPostType, __ {
+            public array $addedActions = [];
+            public array $registeredPostTypes = [];
+            public function addAction(string $hookName, callable $callback, int $priority = 10, int $acceptedArgs = 1): true
+            {
+                $this->addedActions[$hookName][] = [$priority, $callback];
+                return true;
+            }
+
+            public function registerPostType(string $postType, array|string $args = []): WP_Post_Type|WP_Error
+            {
+                $this->registeredPostTypes[$postType] = $args;
+                return new \WP_Post_Type([]);
+            }
+            
+            public function __(string $text, string $domain = 'default'): string
+            {
+                return $text;
+            }
+        };
+    }
+}
