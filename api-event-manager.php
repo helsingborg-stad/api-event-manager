5a15dc92c621330520be2049842f765f62a22c0a
GitHub Action <>
2026-01-29 10:06:53 +0000

    build: update package file versions to 3.12.18

--- api-event-manager.php
+++ api-event-manager.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Event Manager
  * Plugin URI:        https://github.com/helsingborg-stad/api-event-manager
  * Description:       Creates a api that may be used to manage events
- * Version: 3.12.17
+ * Version: 3.12.18
  * Author:            Thor Brink, Sebastian Thulin @ Helsingborg Stad
  * Author URI:        https://github.com/helsingborg-stad
  * License:           MIT
