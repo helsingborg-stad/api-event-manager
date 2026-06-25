d1eb93d252f587e61b387ede446d9ee905336073
Thor Brink <thor.brink@helsingborg.se>
2026-06-25 08:20:40 +0000

    fix: add href to user count on org terms

--- source/php/Organizations/TaxonomyUserCountColumn.php
+++ source/php/Organizations/TaxonomyUserCountColumn.php
@@ -4,10 +4,12 @@ namespace EventManager\Organizations;
 
 use EventManager\HooksRegistrar\Hookable;
 use WpService\Contracts\__;
+use WpService\Contracts\AdminUrl;
+use WpService\Contracts\EscUrl;
 
 class TaxonomyUserCountColumn implements Hookable
 {
-    public function __construct(private \WpService\Contracts\AddFilter&__ $wpService, private string $taxonomy)
+    public function __construct(private \WpService\Contracts\AddFilter&__&AdminUrl&EscUrl $wpService, private string $taxonomy)
     {
     }
 
@@ -26,15 +28,32 @@ class TaxonomyUserCountColumn implements Hookable
     public function populateUserCountColumn(string $content, string $column_name, int $term_id): string
     {
         if ($column_name === 'user_count') {
-            return (string) $this->getUserCountForTerm($term_id);
+            return (string) $this->getColumnOutput($term_id);
         }
 
         return $content;
     }
 
+    private function getColumnOutput(int $term_id): string
+    {
+        $count = $this->getUserCountForTerm($term_id);
+
+        if ($count === 0) {
+            return '0';
+        }
+
+        return '<a href="' . $this->getUserTableHref($term_id) . '">' . $count . '</a>';
+    }
+
+    private function getUserTableHref(int $termId): string
+    {
+        $href = $this->wpService->adminUrl('users.php?organization=' . $termId);
+
+        return $this->wpService->escUrl($href);
+    }
+
     private function getUserCountForTerm(int $termId): int
     {
-        // a:1:{i:0;s:3:"151";}
         $query = new \WP_User_Query([
             'meta_query' => [
                 [
